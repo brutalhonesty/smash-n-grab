24199fee0f6e1d96fb2eb3e2323a60a9e5607a44
Adam Schodde <volvox@cloudy.local>
2014-09-15 01:04:44 -0700

    Fixed spacing for admin file.

--- src/admin.js
+++ src/admin.js
@@ -3,20 +3,20 @@ var nano = require('nano')('http://localhost:5984');
 db = nano.use('smash_n_grab');
 // _deleted
 db.view('smash_n_grab', 'all', {reduce: false}, function (error, reply) {
+  if(error) {
+    return console.log(error);
+  }
+  var codeDocs = [];
+  for (var i = 0; i < reply.rows.length; i++) {
+    var code = reply.rows[i].value;
+    code._deleted = true;
+    codeDocs.push(code);
+  }
+  db.bulk({docs: codeDocs}, function (error, reply) {
     if(error) {
       return console.log(error);
     }
-    var codeDocs = [];
-    for (var i = 0; i < reply.rows.length; i++) {
-      var code = reply.rows[i].value;
-      code._deleted = true;
-      codeDocs.push(code);
-    }
-    db.bulk({docs: codeDocs}, function (error, reply) {
-      if(error) {
-        return console.log(error);
-      }
-      console.log(reply);
-      console.log('All codes deleted.');
-    });
- });
+    console.log(reply);
+    console.log('All codes deleted.');
+  });
+});
